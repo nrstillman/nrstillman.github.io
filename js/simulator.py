b66cbc32f46315b60bbc18ea8e2b204677deb959
nrstillman <namid.stillman@gmail.com>
2023-10-09 22:42:09 +0100

    add publications and edits to homepage

--- js/simulator.py
+++
@@ -1,8 +0,0 @@
-def find_nearest_neighbours(positions, N):
-    neighbours = {}
-    for i in range(N):
-        distances = np.linalg.norm(positions - positions[i], axis=1)
-        neighbour_indices = np.where(distances < 1 + 2)[0]
-        neighbour_indices = neighbour_indices[neighbour_indices != i]
-        neighbours[i] = list(neighbour_indices)
-    return neighbours
